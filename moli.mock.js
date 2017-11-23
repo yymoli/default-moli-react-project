e2e6bd041ebbb4e7efd93c56cc4bc8e102b4ccfe
yaoleib <yaoleib@yonyou.com>
2017-11-23 19:58:26 +0800

    1、删除配置文件

--- moli.mock.js
+++ moli.mock.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "GET": [
-    {"/userlink/getMyCorpUser": "./mock/api/contacts/getMyCorpUser.json"},
-      {"/userlink/getMyTableList": "./mock/api/contacts/getMyTableList.json"},
-    {"/userlink/getFollowed": "./mock/api/contacts/getFollowed.json"},
-    {"/userlink/getContactsDetails": "./mock/api/contacts/getContactsDetails.json"},
-      {"/userlink/header": "./mock/api/header/header.json"},
-  ],
-  "POST": []
+    "GET": [
+        {"/userlink/getMyCorpUser": "./mock/api/contacts/getMyCorpUser.json"},
+        {"/userlink/getMyTableList": "./mock/api/contacts/getMyTableList.json"},
+        {"/userlink/getFollowed": "./mock/api/contacts/getFollowed.json"},
+        {"/userlink/getContactsDetails": "./mock/api/contacts/getContactsDetails.json"},
+        {"/userlink/header": "./mock/api/header/header.json"},
+    ],
+    "POST": []
 }
